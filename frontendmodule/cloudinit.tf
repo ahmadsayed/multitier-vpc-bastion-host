fd1c65bb4b3dcf7fe5bcb8c99e0c6a9c783015ba
Ahmed Sayed <Ahmed.Sayed.Hassan@ibm.com>
2021-04-24 03:27:19 +0800

    Update cloudinit.tf

--- frontendmodule/cloudinit.tf
+++ frontendmodule/cloudinit.tf
@@ -10,6 +10,8 @@ package_upgrade: false
 packages:
 - httpd
 runcmd:
+- 'yum update'
+- 'yum install httpd -y'
 - 'cp /usr/share/httpd/noindex/index.html /var/www/html' 
 - 'systemctl start httpd'
 final_message: "The system is finally up, after $UPTIME seconds"
