c568609fc4080337d92efd56fe9591685ac44f88
Ahmed Sayed <Ahmed.Sayed.Hassan@ibm.com>
2021-04-25 00:34:36 +0800

    Update cloudinit.tf

--- frontendmodule/cloudinit.tf
+++ frontendmodule/cloudinit.tf
@@ -9,9 +9,11 @@ manage_etc_hosts: true
 package_upgrade: false
 packages:
 - httpd
+- python2-pip
 runcmd:
 - 'cp /usr/share/httpd/noindex/index.html /var/www/html' 
 - 'systemctl start httpd'
+-  pip install certifi
 final_message: "The system is finally up, after $UPTIME seconds"
 EOF
 
